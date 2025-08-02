d9421f6a69647d04cce3a12cd264a3054609b127
mzkhawar <mzkhawar775@gmail.com>
2025-08-02 13:01:42 -0500

    db/env setup

---
+++ src/main/java/dev/mzkhawar/cornerperks/CornerperksApplication.java
@@ -0,0 +1,13 @@
+package dev.mzkhawar.cornerperks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornerperksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CornerperksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mzkhawar/cornerperks/CornerperksApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mzkhawar.cornerperks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornerperksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
